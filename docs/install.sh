30afd3b5a39c3ad2f7b7bd6180a975099e15e25c
Bob Bisantz <sl45082@hotmail.com>
2024-11-15 19:21:37 -0500

    installer stuff v1

---
+++ docs/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+cat banner.txt
+echo "hit ENTER to install or CTRL-C to abort: ";read x
+
+#assuming hdmi port 0 is here monitor is
+export DISPLAY=:2
+
+# 1. make sure we are in good directory
+mydir=`basename "$PWD"`
+if [[ "${mydir}" -ne "Documents" ]];
+then
+	echo "Please run install from ~/Documents directory, existing so you can retry."
+	exit
+fi
+
+wget  https://sl45082.github.io/basic/d12rpi5wapp1.0.zip -o log
+unzip -l d12rpi5wapp1.0.zip
+echo "hit ENTER if you see a list of files otherwise CTRL-C to abort: ";read x
+
+unzip -v d12rpi5wapp1.0.zip
+
+# 2. install the cron
+sudo cp -v weather.cron /var/spool/cron/crontabs/admin
+
+
+# 3. make sure the .sh files are executable
+chmod +x *.sh
+
+# 4. append auto launch to end of wayfire.ini
+cat wayfire.ini  >> ~/.config/wayfire.ini
+
+sleep 3
+echo "Rebooting now...."
+reboot
+
