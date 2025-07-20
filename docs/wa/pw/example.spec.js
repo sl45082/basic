c934517de6aa2d8b3824acb1c82c5dde66aa13c7
bob <sl45082@hotmail.com>
2025-07-20 15:21:09 -0400

    checkin resources: 2025-07-20 15:21:09

--- docs/wa/pw/example.spec.js
+++ docs/wa/pw/example.spec.js
@@ -1,47 +1,21 @@
 
-
 const { test, expect } = require('@playwright/test');
-
 
 const { chromium } = require('playwright');
 
-test('capture screenshots', async ({ page }) => {
+test('capture tide', async ({ page }) => {
  const browser = await chromium.launch({
     headless: true,
     args: ['--kiosk']
   });
 
   await page.goto('https://www.tide-forecast.com/locations/Oregon-Inlet-North-Carolina/tides/latest');
-  title = await page.title();
+  const title = await page.title();
   await page.evaluate(() => {
     window.scrollTo(0, 375); // Scroll down y pixels
   });
   // await page.waitForTimeout(5000); // pauses for 3 second
   await page.screenshot({ path: '/tmp/wa/tides.png' });
-
-
-
-  await page.goto('https://www.star.nesdis.noaa.gov/goes/sector_band.php?sat=G19&sector=eus&band=11&length=71&dim=1');
-  title = await page.title();
-  await page.evaluate(() => {
-    window.scrollTo(0, 50); // Scroll down y pixels
-  });
-  await page.screenshot({ path: '/tmp/wa/noaa.png' });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
 
