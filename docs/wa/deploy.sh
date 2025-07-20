e7d3b8adc7e58c843445b11ca3131e7cc8318544
bob bisantz <sl45082@hotmail.com>
2025-07-20 11:29:44 -0400

    weather poc1, testing scripts

--- docs/wa/deploy.sh
+++ docs/wa/deploy.sh
@@ -1,9 +1,13 @@
 
 
-IMAGEDIR=~/Documents/surftemp
+IMAGEDIR=/tmp/wa
 export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy_key'
-#
-# get all the images into the right dir
+
+# generate an image for each page during test script
+
+
+ 
+# get all the images into the right dir and publish them
 cp ${IMAGEDIR}/* .
 git add . 
 git commit -m "checkin resources: $(date '+%Y-%m-%d %H:%M:%S')"
