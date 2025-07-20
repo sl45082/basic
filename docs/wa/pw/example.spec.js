637b595ca1ccb2b868fc49e44a0cc0e06af4c8c9
bob <sl45082@hotmail.com>
2025-07-20 15:16:48 -0400

    checkin resources: 2025-07-20 15:16:48

--- docs/wa/pw/example.spec.js
+++ docs/wa/pw/example.spec.js
@@ -22,7 +22,7 @@ test('capture screenshots', async ({ page }) => {
 
 
   await page.goto('https://www.star.nesdis.noaa.gov/goes/sector_band.php?sat=G19&sector=eus&band=11&length=71&dim=1');
-  const title = await page.title();
+  title = await page.title();
   await page.evaluate(() => {
     window.scrollTo(0, 50); // Scroll down y pixels
   });
