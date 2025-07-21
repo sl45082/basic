b92000803cfd0b5d62a9ad9bce847868db9e86e0
bob <sl45082@hotmail.com>
2025-07-21 18:26:32 -0400

    lwwa deploy script.

--- docs/wa/deploy.sh
+++ docs/wa/deploy.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 
+
+PATH=$PATH:/home/admin/.nvm/versions/node/v24.4.1/bin
 
 IMAGEDIR=/tmp/wa
 export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy_key'
@@ -8,11 +11,15 @@ rm -f ${IMAGEDIR}/*.png
 # generate an image for each page during test script
 cd /home/admin/Documents/basic/docs/wa/pw
 
+date >> cron.log
+whereis npx >> cron.log
 npx playwright test --trace on
  
 # get all the images into the right dir and publish them
 cp ${IMAGEDIR}/* .
-git status
+
+whereis git >> cron.log
+git status >> cron.log
 
 
 #  REMOVE THIS
