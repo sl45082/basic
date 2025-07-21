e8667a0d6c1e6abe14d7e6358d52434df00b9506
bob <sl45082@hotmail.com>
2025-07-21 17:41:44 -0400

    fixing lwwa surfchex test specs .

--- docs/wa/pw/surfchex1.spec.js
+++ docs/wa/pw/surfchex1.spec.js
@@ -12,10 +12,17 @@ test('capture surfchex1', async ({ page }) => {
     ]
   });
 
-  await page.goto('https://surfchex.intelliweather.net/imagery/Surfchex/rad_nc_moreheadcity_640x480.htm');
+
+  await page.goto('https://surfchex.intelliweather.net/imagery/Surfchex/rad_nc_moreheadcity_640x480.htm', {
+    waitUntil: 'networkidle',
+    timeout: 60000  // prob dont need this
+  });
+
   const title = await page.title();
+
   await page.evaluate(() => {
     window.scrollTo(0, 3); // Scroll down y pixels
+    document.body.style.zoom = '2.0'; // 2x mag
   });
   // await page.waitForTimeout(5000); // pauses for 3 second
   await page.screenshot({ path: '/tmp/wa/surfchex1.png' });
--- docs/wa/pw/surfchex2.spec.js
+++ docs/wa/pw/surfchex2.spec.js
@@ -8,14 +8,18 @@ test('capture surfchex2', async ({ page }) => {
     headless: true,
     args: [
 	'--kiosk',
-	'--window-size=1920,1080'
+	'--window-size=1920,2200'
     ]
   });
 
-  await page.goto('https://surfchex.intelliweather.net/imagery/Surfchex/satrad_reg_640x480.htm');
+  await page.goto('https://surfchex.intelliweather.net/imagery/Surfchex/satrad_reg_640x480.htm', {
+    waitUntil: 'networkidle',
+    timeout: 60000  // prob dont need this
+  });
   const title = await page.title();
   await page.evaluate(() => {
     window.scrollTo(0, 5); // Scroll down y pixels
+    document.body.style.zoom = '2.0'; // 2x mag
   });
   // await page.waitForTimeout(5000); // pauses for 3 second
   await page.screenshot({ path: '/tmp/wa/surfchex2.png' });
