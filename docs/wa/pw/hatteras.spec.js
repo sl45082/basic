b202c1371a260919570df220ad1dd7184662e20d
bob <sl45082@hotmail.com>
2025-07-20 21:31:06 -0400

    lwwa script,more testcase updates.

--- docs/wa/pw/hatteras.spec.js
+++ docs/wa/pw/hatteras.spec.js
@@ -6,14 +6,21 @@ const { chromium } = require('playwright');
 test('capture hatteras surf', async ({ page }) => {
  const browser = await chromium.launch({
     headless: true,
-    args: ['--kiosk']
+    args: [
+	'--kiosk',
+        '--autoplay-policy=no-user-gesture-required',
+        '--use-fake-ui-for-media-stream',
+        '--use-fake-device-for-media-stream',
+        '--disable-gpu',
+        '--mute-audio'
+    ]
   });
 
   await page.goto('https://www.surfchex.com/cams/hatteras-web-cam/');
   const title = await page.title();
-  await page.evaluate(() => {
-    window.scrollTo(0, 100); // Scroll down y pixels
-  });
+  //await page.evaluate(() => {
+  //  window.scrollTo(0, 100); // Scroll down y pixels
+  //});
   // await page.waitForTimeout(5000); // pauses for 3 second
   await page.screenshot({ path: '/tmp/wa/hatteras.png' });
 
