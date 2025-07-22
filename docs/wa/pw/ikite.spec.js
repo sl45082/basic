73a59ef2363d4388322207909e2aa1f1b95ae3a5
bob <sl45082@hotmail.com>
2025-07-22 07:47:12 -0400

    lwwa ikite-wind update.

--- docs/wa/pw/ikite.spec.js
+++ docs/wa/pw/ikite.spec.js
@@ -12,7 +12,7 @@ test('capture ikite', async ({ page }) => {
     ]
   });
 
-  await page.goto(' https://wx.ikitesurf.com/search/35.158/-75.282/8', {
+  await page.goto('https://www.windy.com/35.241/-75.558?35.082,-75.460,8', {
     waitUntil: 'networkidle',
     timeout: 60000  // prob dont need this
   });
@@ -20,10 +20,16 @@ test('capture ikite', async ({ page }) => {
   const title = await page.title();
 
   await page.evaluate(() => {
-    window.scrollTo(0, 3); // Scroll down y pixels
+    window.scrollTo(0, 100); // Scroll down y pixels
     document.body.style.zoom = '1.5'; // 1.5x mag
   });
-  // await page.waitForTimeout(5000); // pauses for 3 second
+
+  await page.waitForTimeout(5000); // pauses for 3 second
+//  await page.waitForSelector('div.jw-marker.t1.p1.marker[title="Avon Sound"]', {
+//    state: 'visible',
+//    timeout: 60000 // optional timeout (10 seconds)
+//  });
+ 
   await page.screenshot({ path: '/tmp/wa/ikite.png' });
 
 });
