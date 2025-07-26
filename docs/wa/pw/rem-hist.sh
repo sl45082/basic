2b6aed44704b487d6bdc236305c0cbb6973a00bc
bob <sl45082@hotmail.com>
2025-07-26 11:59:46 -0400

    still more lwwa history repon clan up.

--- docs/wa/pw/rem-hist.sh
+++ docs/wa/pw/rem-hist.sh
@@ -1,10 +1,15 @@
-cd basic/docs/wa/pw
+#!/bin/bash
+
+#cd basic/docs/wa/pw
+
+export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy_key'
 
 for i in `ls *.png *.mp3 *.gif`
 do
-echo "Working on ${i} \n"
-#git filter-repo --path ${i} --invert-paths --force
+	date
+	echo "Working on ${i} \n"
+	git filter-repo --path ${i} --invert-paths --force
+	git push --force --all
 done
 
-#git push --force
 
