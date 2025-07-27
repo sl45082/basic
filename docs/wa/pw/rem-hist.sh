09b8967f0567991864c12596ece7e376abf5b788
bob <sl45082@hotmail.com>
2025-07-27 12:16:28 -0400

    hist remover script

--- docs/wa/pw/rem-hist.sh
+++ docs/wa/pw/rem-hist.sh
@@ -3,13 +3,16 @@
 #cd basic/docs/wa/pw
 
 export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy_key'
+git remote add origin git@github.com:sl45082/basic.git
+git checkout main
 
 for i in `ls *.png *.mp3 *.gif`
 do
 	date
 	echo "Working on ${i} \n"
+	git remote add origin git@github.com:sl45082/basic.git
 	git filter-repo --path ${i} --invert-paths --force
-	git push --force --all
+	git push origin main:main --force
 done
 
 
